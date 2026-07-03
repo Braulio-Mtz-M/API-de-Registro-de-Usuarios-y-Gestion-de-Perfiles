bef1635083c39b02912871b2f76c75e7dc833e8a
Braulio Martinez <brauliomarmo@gmail>
2026-07-02 20:00:20 -0600

    Coneccion a la base de datos

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const connectDB = require('./src/config/database');
 
 const app = express();
 const PORT = 5100;
+
+// DB connection
+connectDB();
+
 
 app.listen(PORT, () => {
     console.log('Hello World');
